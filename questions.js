3854e2c8a0b8cdbc2a6047a98741c5b51aed9c89
Werner Staeblein <wstablein@hotmail.com>
2024-02-27 14:24:25 +0000

    add question.js with prepared initial questions. Check that questions.js is connected with console. Add questions.js in script tag in HTML

---
+++ questions.js
@@ -0,0 +1,34 @@
+const quizData = [
+    {
+      question: "What is the purpose of the 'let' keyword in JavaScript?",
+      choices: [
+        "Define a constant variable",
+        "Declare a variable with block scope",
+        "Declare a variable with global scope",
+        "Define a function",
+      ],
+      correctAnswer: 1,
+    },
+    
+    {
+      question: "What does the '=== operator' do in JavaScript?",
+      choices: [
+        "Checks for equality with type coercion",
+        "Checks for equality without type coercion",
+        "Assigns a value to a variable",
+        "Compares two strings lexicographically",
+      ],
+      correctAnswer: 1,
+    },
+    
+    {
+      question:
+        "Which of the following is a method used to iterate over the elements of an array in JavaScript?",
+      choices: ["for loop", "while loop", "foreach loop", "map function"],
+      correctAnswer: 2,
+    }
+  ];
+  
+  console.log(quizData.length)
+  
+  
