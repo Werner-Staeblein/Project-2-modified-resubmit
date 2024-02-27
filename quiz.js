3fa37b2cc161d19de8732bbe42270e6d5c6fdc32
Werner Staeblein <wstablein@hotmail.com>
2024-02-27 14:52:26 +0000

    DOMContentLoaded. Ensure that instructions and start quizBtn disappear once clicked. Header and logo to stay. Check functionality of code. Works

--- quiz.js
+++ quiz.js
@@ -5,3 +5,18 @@ const retryButton = document.getElementById('retry');
 const showAnswerButton = document.getElementById('showAnswer');
 
 import quizData from './questions.js';
+
+document.addEventListener("DOMContentLoaded", function () {
+    const startQuizBtn = document.getElementById('startQuizBtn');
+    const instructionsDiv = document.getElementById('instructions');
+    const quizContainer = document.querySelector('.container');
+  
+    startQuizBtn.addEventListener('click', function () {
+      startQuizBtn.style.display = 'none';
+      instructionsDiv.style.display = 'none';
+      quizContainer.style.display = 'block';
+    });
+  
+});
+  
+  
