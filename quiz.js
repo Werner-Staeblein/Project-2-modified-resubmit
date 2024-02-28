316466e2b262745bb800848eddc35d4a4999aad6
Werner Staeblein <wstablein@hotmail.com>
2024-02-28 07:40:13 +0000

    Check start of game with console.log. Define counter variables. Include function to randomize array taken from Stackoverflow.Define displayQuestion() function. Confirmation via consolethat quiz start works

--- quiz.js
+++ quiz.js
@@ -15,8 +15,33 @@ document.addEventListener("DOMContentLoaded", function () {
       startQuizBtn.style.display = 'none';
       instructionsDiv.style.display = 'none';
       quizContainer.style.display = 'block';
+      startGame();
     });
   
 });
-  
-  
+
+/**
+ * Function to randomize the questions. This function picks a random element
+ * from each original array and excludes it from the next draw.
+ * Function taken from Stackoverflow
+ */
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
+
+function startGame() {
+  console.log("Game started!");
+
+  selectedQuestions = shuffleArray(quizData).slice(0, 10);
+
+  currentQuestion = 0;
+  score = 0;
+  incorrectAnswers = [];
+  displayQuestion();
+}
+
+
